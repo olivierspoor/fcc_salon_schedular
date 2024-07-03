15c9b6b24a0de0249f900077e9eb60f064a74da8
Olivier Spoor <51018665+olivierspoor@users.noreply.github.com>
2024-07-03 16:22:47 +0200

    feat: display main menu

--- salon.sh
+++ salon.sh
@@ -3,3 +3,14 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ Salon Appointment Scheduler ~~~~~\n"
+
+MAIN_MENU() {
+  echo -e "How may I help you?\n"
+
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+
+  echo $SERVICES | sed -E 's/([0-9])/\n\1/g;s/ \|/)/g'
+
+}
+
+MAIN_MENU
